f5234c14b8073200607b12f821b09a9eada038bb
Quarrtul-Ain <quarrtul.bscs2307@uoch.edu.pk>
2026-06-11 22:48:14 +0500

    ecplaining javascript

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const textarea = document.getElementById("textarea");
-const tagsContainer = document.getElementById("tags");
+const textarea = document.getElementById("textarea");// HTML se textarea element ko uski id ki madad se select kar raha hai
+const tagsContainer = document.getElementById("tags"); //selecting div container thats id ia "tags"
 
+// Jab user keyboard ki koi key chhore (keyup event), ye function chalega
 textarea.addEventListener("keyup", (e) => {
-    createTags(e.target.value);
+    createTags(e.target.value); // Textarea ke andar jo text hai usay createTags function ko bhej raha hai
 
+     // Check kar raha hai ke user ne Enter key dabayi hai ya nahi
     if (e.key === "Enter") {
-        e.preventDefault();
+        e.preventDefault();   // Enter ka default action (new line banana) rok deta hai
 
         setTimeout(() => {
-            e.target.value = "";
+            e.target.value = "";   // Textarea ko khali kar deta hai
         }, 10);
 
-        alert("Choices entered successfully!");
+        alert("Choices entered successfully!");        // User ko popup message show karta hai
     }
 });
 
+// Function banaya gaya hai jo tags create karega
 function createTags(input) {
     // Split text using commas
     const tags = input
